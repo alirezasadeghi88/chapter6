f42efab889e59addd6205fc705b65a8a8e72c4ad
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2025-10-17 05:22:21 -0700

    Refactoring

--- Increment.java
+++ Increment.java
@@ -2,13 +2,14 @@ public class Increment {
 	public static void main(String[] args) {
 		int x =1;
 		System.out.println("Befor the call, x is " + x);
-		incremet(x);
+		x = incremet(x);
 		System.out.println("After the call, x is " + x);
 		
 	}
-	public static void incremet(int n) {
+	public static int incremet(int n) {
 		n++;
 		System.out.println("n inside the method is " + n);
+		return n;
 		
 	}
 }
